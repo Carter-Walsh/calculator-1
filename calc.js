f59b30eda85799da67f45e01b02812a6dc84649c
Carter Walsh <Cwalsh616@gmail.com>
2021-06-14 13:51:05 -0500

    added further study logic

--- calc.js
+++ calc.js
@@ -26,11 +26,14 @@ reader.question("What would you like to calculate?", function(input){
     } else if (mathSymbol === "/") {
         console.log(num1/num2);
     } else if (mathSymbol == "^") {
-        console.log(Math.sqrt(num1));
-        console.log(Math.sqrt(num2));
+        console.log(Math.sqrt(num1), Math.sqrt(num2));
+    } else if (mathSymbol === "^2") {
+        console.log(Math.pow(num1, 2), Math.pow(num2, 2));
+    } else if (mathSymbol === "^3") {
+        console.log(Math.pow(num1, 3), Math.pow(num2, 3));
+    } else if (mathSymbol === "%") {
+        console.log(num1 % num2);
     }
-
-    
 
 	// This line closes the connection to the command line interface.
 	reader.close()
